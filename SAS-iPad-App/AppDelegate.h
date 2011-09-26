67d95c9ac3dd564adf70647a5a577b7a30688199
Henning Muentinga <henning.muentinga@neveling.net>
2011-09-26 14:52:07 +0200

    initial

---
+++ SAS-iPad-App/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SAS-iPad-App
+//
+//  Created by Oliver  Neveling on 26.09.11.
+//  Copyright (c) 2011 neveling.net GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
