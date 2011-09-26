c66e0bdd36f3b44489ed48ac1c60a27582c9498b
Henning Muentinga <henning.muentinga@neveling.net>
2011-09-26 18:20:49 +0200

    d

---
+++ SAS-iPad-App/Apartment.m
@@ -0,0 +1,30 @@
+//
+//  Apartment.m
+//  SAS-iPad-App
+//
+//  Created by Oliver  Neveling on 26.09.11.
+//  Copyright (c) 2011 neveling.net GmbH. All rights reserved.
+//
+
+#import "Apartment.h"
+
+
+@implementation Apartment
+
+@dynamic exid;
+@dynamic name;
+@dynamic rooms;
+@dynamic bed_rooms;
+@dynamic living_area;
+@dynamic persons;
+@dynamic max_persons;
+@dynamic zip_code;
+@dynamic city;
+@dynamic street;
+@dynamic groupID;
+@dynamic wohnlageID;
+@dynamic longitude;
+@dynamic latitude;
+@dynamic attributes;
+
+@end
