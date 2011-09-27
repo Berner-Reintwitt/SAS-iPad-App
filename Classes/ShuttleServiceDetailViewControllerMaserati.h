1a078f6df9849635bb319c491d12e066b0250351
Johannes Böhler <johannes@Johannes-Bohlers-MacBook-Pro.local>
2011-09-27 17:52:45 +0200

    1

---
+++ Classes/ShuttleServiceDetailViewControllerMaserati.h
@@ -0,0 +1,13 @@
+//
+//  ShuttleServiceDetailViewControllerMaserati.h
+//  TabBarWithSplitView
+//
+//  Created by Johannes Böhler on 27.09.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShuttleServiceDetailViewControllerMaserati : UIViewController
+
+@end
---
+++ ShuttleServiceRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShuttleServiceRootViewController.h
+//  TabBarWithSplitView
+//
+//  Created by Johannes Böhler on 27.09.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShuttleServiceRootViewController : UIViewController
+
+@end
