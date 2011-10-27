fb86c455992ee819566ca3182ba2b6a918b84b93
Johannes Böhler <johannes.boehler@neveling.net>
2011-10-27 17:38:09 +0200

    1

--- Classes/SuchergebnisseDomizil.m
+++ Classes/SuchergebnisseDomizil.m
@@ -9,6 +9,10 @@
 #import "SuchergebnisseDomizil.h"
 
 @implementation SuchergebnisseDomizil
+@synthesize mapView;
+@synthesize table;
+@synthesize myscrollView;
+@synthesize pageControl;
 @synthesize scrollView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -31,18 +35,42 @@
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
-{   scrollView.frame = CGRectMake(0, 325,1024 , 748);
+{   scrollView.frame = CGRectMake( 0,0,1024 , 748);
     
     //---set the content size of the scroll view---
-    [scrollView setContentSize:CGSizeMake(1600, 748)];
+    [scrollView setContentSize:CGSizeMake(1900, 748)];
+    NSArray *colors = [NSArray arrayWithObjects:[UIColor redColor], [UIColor greenColor], [UIColor blueColor], [UIColor blackColor],nil];
+    for (int i = 0; i < colors.count; i++) {
+        CGRect frame;
+        frame.origin.x = self.myscrollView.frame.size.width * i;
+        frame.origin.y = 0;
+        frame.size = self.myscrollView.frame.size;
+        
+        UIView *subview = [[UIView alloc] initWithFrame:frame];
+        subview.backgroundColor = [colors objectAtIndex:i];
+        [self.myscrollView addSubview:subview];
+        pageControl.numberOfPages=[colors count];
+        [subview release];
+        
+        // Do any additional setup after loading the view from its nib.
+    }
+    self.myscrollView.contentSize = CGSizeMake(self.myscrollView.frame.size.width * colors.count, self.myscrollView.frame.size.height);
     
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)viewDidUnload
-{
+{   
+    
+    [self setMyscrollView:nil];
+    [self setPageControl:nil];
+    [self setMyscrollView:nil];
+    [self setPageControl:nil];
+    [super viewDidUnload];
     [self setScrollView:nil];
+    [self setMapView:nil];
+    [self setTable:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -56,13 +84,69 @@
 
 
 - (IBAction)scroll:(id)sender {
-    NSLog(@"Hallo");
-    UIScrollView *scrollview=  (UIScrollView *)self.view;
-    [scrollview scrollRectToVisible:CGRectMake(340, 750, 1024, 768) animated:YES];
+    
+    CGRect frame;
+    frame.origin.x = mapView.frame.size.width+50;
+    //frame.origin.y = 0;
+    frame.origin.y=0;
+    frame.size = CGSizeMake(1024, 748);
+    [self.scrollView scrollRectToVisible:frame animated:YES];
+    
+   // [scrollview scrollRectToVisible:CGRectMake(340, 750, 1024, 768) animated:YES];
     
 }
+
+- (IBAction)srollBack:(id)sender {
+    
+    CGRect frame;
+    frame.origin.x = 0;
+    
+    frame.origin.y=0;
+    frame.size = CGSizeMake(1024, 748);
+    [self.scrollView scrollRectToVisible:frame animated:YES];
+}
 - (void)dealloc {
+    
+    [myscrollView release];
+    [pageControl release];
+    [super dealloc];
+
     [scrollView release];
+    [mapView release];
+    [table release];
+    [myscrollView release];
+    [pageControl release];
     [super dealloc];
 }
+- (IBAction)switchFrame:(id)sender {
+    
+    scrollView.frame=CGRectMake(mapView.frame.size.width, 0, 1024, 748);
+    
+}
+
+
+
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    // Update the page when more than 50% of the previous/next page is visible
+    CGFloat pageWidth = myscrollView.frame.size.width;
+    int page = floor((myscrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    pageControl.currentPage = page;
+    
+}
+
+
+- (IBAction)changePage:(id)sender {
+    CGRect frame;
+    frame.origin.x = self.myscrollView.frame.size.width * self.pageControl.currentPage;
+    frame.origin.y = 0;
+    frame.size = self.myscrollView.frame.size;
+    [self.myscrollView scrollRectToVisible:frame animated:YES];
+    
+    
+}
+
+
+
+
 @end
